6ed516c9ec3e78727734ac87123fafa98f8c66eb
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-19 13:50:56 -0600

    add resolvers to server.js

--- server.js
+++ server.js
@@ -14,9 +14,59 @@ var schema = buildSchema(`
     shark: String
   }
 `);
+// Sample users
+var users = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: '21',
+    shark: 'Great White Shark',
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: '22',
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: '23',
+    shark: 'Hammerhead Shark'
+  },
+  {
+    id: 4,
+    name: 'Joseph',
+    age: '23',
+    shark: 'Tiger Shark'
+  },
+  {
+    id: 5,
+    name: 'Joy',
+    age: '25',
+    shark: 'Hammerhead Shark'
+  }
+];
+
+// Return a single user
+var getUser = function(args) {
+  var userID = args.id;
+  return users.filter(user => user.id == userID)[0];
+}
+
+// Return a list of users
+var retrieveUsers = function(args) {
+  if (args.shark) {
+    var shark = args.shark;
+    return users.filter(user => user.shark === shark);
+  } else {
+    return users;
+  }
+}
 
 var root = {
-  hello: () => 'Hello, world!'
+  user: getUser,
+  users: retrieveUsers
 };
 
 var app = express();
